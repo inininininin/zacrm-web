6aa2eee206295be4adc9d05683da13f5418c444b
xuxk13046567620 <839362863@qq.com>
2020-04-27 16:56:10 +0800

    5454

--- js/public.js
+++ js/public.js
@@ -387,6 +387,21 @@ function countDownSec(starttimes) {
         //$('.timespan').html(day + "天" + hour + "小时" + minute + "分钟" + seconds + "秒");
     }, 1000);
 }
+
+
+
+function changeDateFormat(cellval) {
+				var dateVal = cellval + "";
+				if (cellval != null) {
+					var date = new Date(parseInt(dateVal.replace("/Date(", "").replace(")/", ""), 10));
+					var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+					var currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+					var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+					var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+					var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+					return date.getFullYear() + "-" + month + "-" + currentDate + " " + hours + ":" + minutes + ":" + seconds;
+				}
+			}
 //rem自适应
 //var dpr, rem, scale;
 //var docEl = document.documentElement;
